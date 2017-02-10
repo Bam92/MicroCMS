b61abbb1160187b7fef992fa3256ed020d02998b
Bam92 <me@abelmbula.url.ph>
2017-02-10 11:52:22 +0200

    refactoring of the app followed MVC model

--- index.php
+++ index.php
@@ -1,24 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8" />
-	<link rel="stylesheet" type="text/css" href="microcms.css">
-	<title>MicroCMS - Home</title>
-</head>
-<body>
-	<header><h1>MicroCMS</h1></header>
-	<?php
-	$bdd = new PDO('mysql:host=localhost;dbname=microcms', 'microcms_user', '$db@microcms_user');
-	$articles = $bdd->query('SELECT * FROM t_article ORDER BY art_id DESC');
-	foreach ($articles as $article): ?>
-	<article>
-		<h2><?php echo $article['art_title']; ?></h2>
-		<p><?php echo $article['art_content']; ?></p>
-	</article>
-	<?php endforeach ?>
+<?php
 
-	<footer class="footer">
-		<a href="#">MicroCMS</a> is a minimalistic CMS built as a showcase for modern PHP development by Abel Mbula
-	</footer>
-</body>
-</html>
+
+	require 'model.php';
+	$articles = getArticles();
+	require 'view.php' ;
