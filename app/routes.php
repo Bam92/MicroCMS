be6681e946b82b7eb357c393c19025d021d9c954
Bam92 <me@abelmbula.url.ph>
2017-02-14 23:19:24 +0200

    dao service used in route.php

--- app/routes.php
+++ app/routes.php
@@ -1,19 +1,13 @@
 <?php
 
 // Home page
-$app->get('/', function () {
-	require '../src/model.php';
-	$articles = getArticles();
+$app->get('/', function () use ($app) {
+	
+	$articles = $app['dao.article']->findAll();
 
 	ob_start();	// start buffering HTML output
 	require '../views/view.php';
 	$view = ob_get_clean();	// assign HTML output to $view
 	return $view;
-		require '../src/model.php';
-		$articles = getArticles();
 		
-		ob_start();	// start buffering HTML output
-		require '../views/view.php';
-		$view = ob_get_clean();	// assign HTML output to $view
-		return $view;
 });
