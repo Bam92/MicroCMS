11c5e23e489d1e75cc58abb9af3eb0edcf9f53f8
Bam92 <me@abelmbula.url.ph>
2017-02-14 22:58:51 +0200

    app.php, contains Silex app setup

--- src/model.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Return all articles
-function getArticles() {
-	// Data access
-	$bdd = new PDO('mysql:host=localhost;dbname=microcms', 'microcms_user', '$db@microcms_user');
-	$articles = $bdd->query('SELECT * FROM t_article ORDER BY art_id DESC');
-	return $articles;
-}
